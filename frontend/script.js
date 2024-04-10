2c4c03beadb110943d74e8370efae06730af1322
Atharva082004 <atharvalandge2004@gmail.com>
2024-04-10 12:09:20 +0530

    Update frontend script.js and index.html

--- frontend/script.js
+++ frontend/script.js
@@ -18,6 +18,13 @@ const candidate=document.querySelector('#candidate')
 const addTheCandidate=document.querySelector('#addTheCandidate');
 
 const contractAddress = `0xC04F989ecAc841e8d8921f9BBf339DB57c3aBBda`;
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 const contractABI = [
     {
       "inputs": [],
